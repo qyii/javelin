475161ab5cc8d0eda64e67133450aa321107f73d
xint <apupupupupu@gmail.com>
2019-11-17 18:52:33 +0800

    add template

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const delDir = require('./del')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 delDir('./dist')
 console.log('ok')
@@ -12,6 +13,12 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: '[name]-[chunkhash:8].js'
-  }
+    filename: '[name].[chunkhash:8].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'hololive',
+      template: 'public/index.html'
+    })
+  ]
 }
